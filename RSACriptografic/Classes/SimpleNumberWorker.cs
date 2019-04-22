c9d394a17bbca8f9edc4535973692f3ff63ea8f7
Denis Logovik <denislogovik@gmail.com>
2019-04-22 18:16:25 +0300

    Создан метод для проверки числа на простату

---
+++ RSACriptografic/Classes/SimpleNumberWorker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSACriptografic.Classes
+{
+    class SimpleNumberWorker
+    {
+        public static bool IsPrime(uint num)
+        {
+            if (num == 1) return false;
+            if ((num % 2 == 0) && (num > 2)) return true;
+
+            int divsCount = 1;
+
+            for (int i = 1; i <= num/2; i++)
+            {
+                if (num % i == 0) divsCount++;
+            }
+
+            return divsCount == 2 ? true : false;
+        }
+    }
+}
