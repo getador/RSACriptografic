d049197cc645f1f892b0277041779ac6104fffbc
Slava Kurganov <sgorelchik@mail.ru>
2019-04-22 18:17:05 +0300

    Начало создания класса работника

--- RSACriptografic/Classes/CriptoWorker.cs
+++ RSACriptografic/Classes/CriptoWorker.cs
@@ -8,5 +8,21 @@ namespace RSACriptografic.Classes
 {
     class CriptoWorker
     {
+        uint p;
+        uint q;
+        uint n;
+        uint m;
+        uint d;
+        double e;
+        public CriptoWorker()
+        {
+
+        }
+        public uint P { get => p; set => p = value; }
+        public uint Q { get => q; set => q = value; }
+        public uint N { get => n; set => n = value; }
+        public uint M { get => m; set => m = value; }
+        public uint D { get => d; set => d = value; }
+        public double E { get => e; set => e = value; }
     }
 }
