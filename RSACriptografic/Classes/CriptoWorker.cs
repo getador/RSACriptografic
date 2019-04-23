0965fbea85d7e8d95625b2ee1e293cf44522d71f
Slava Kurganov <sgorelchik@mail.ru>
2019-04-23 17:29:09 +0300

    Готово для тестирования

--- RSACriptografic/Classes/CriptoWorker.cs
+++ RSACriptografic/Classes/CriptoWorker.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace RSACriptografic.Classes
 {
     class CriptoWorker
     {
-        Random random;
         uint p;
         uint q;
         uint n;
         uint m;
         uint d;
-        double e;
+        uint e;
 
-        public CriptoWorker(int maxValue)
+        public CriptoWorker(int maxValue,Random random)
         {
             if (maxValue<2)
             {
@@ -24,39 +25,108 @@ namespace RSACriptografic.Classes
             }
             p = 0;
             q = 0;
-            while (p==0)
+            while (!SimpleNumberWorker.IsPrime(p))
             {
                 p = (uint)random.Next(2,maxValue);
             }
-            while (q==0)
+            while (!SimpleNumberWorker.IsPrime(q))
             {
                 q = (uint)random.Next(2, maxValue);
             }
             n = p * q;
             m = (p - 1) * (q - 1);
-            d = 0;
-            e = FindEelement();
+            d = m;
+            while (!SimpleNumberWorker.IsMutuallyPrimary(d,m))
+            {
+                d = (uint)random.Next((int)m-1);
+            }
+            //for (int i = (int)m-1; i > 1; i--)
+            //{
+            //    if (SimpleNumberWorker.IsMutuallyPrimary((uint)i,m))
+            //    {
+            //        d = (uint)i;
+            //        break;
+            //    }
+            //}
+            e = FindEelement(random);
         }
-
-        public uint FindEelement()
+        /// <summary>
+        /// Шифрование RSA
+        /// </summary>
+        /// <param name="message">Сообщение для зашифровки</param>
+        /// <param name="e">Первый элемент открытого ключа</param>
+        /// <param name="n">Второй элемент открытого ключа</param>
+        /// <returns>Зашифрованое сообщение</returns>
+        public string Encript(string message,uint e,uint n)
+        {
+            message = message.ToLower();
+            string encriptMessage = "";
+            for (int i = 0; i < message.Length; i++)
+            {
+                int index = 0;
+                for (int j = 0; j < Alphabet.alphabet.Length; j++)
+                {
+                    if (message[i]==Alphabet.alphabet[j])
+                    {
+                        index = j;
+                        break;
+                    }
+                }
+                encriptMessage += Convert.ToString((int)(Math.Pow(index, e)%n))+"|";
+            }
+            using (StreamWriter stream = new StreamWriter("1.txt", false))
+            {
+                stream.WriteLine(encriptMessage);
+            }
+            return encriptMessage;
+        }
+        public string ToUnEncript(string message)
+        {
+            string unEncriptMessage = "";
+            string[] arrayElement = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < arrayElement.Length; i++)
+            {
+                //try
+                //{
+                //unEncriptMessage += Convert.ToString((int)(Math.Pow(int.Parse(arrayElement[i]), d) % n)) + "|";
+                unEncriptMessage += Convert.ToString(Alphabet.alphabet[(int)(Math.Pow(int.Parse(arrayElement[i]), d) % n)]);
+                //}
+                //catch (Exception)
+                //{
+                //}                
+            }
+            using (StreamWriter stream = new StreamWriter("2.txt",false))
+            {
+                stream.WriteLine(unEncriptMessage);
+            }
+            return unEncriptMessage;
+        }
+        public uint FindEelement(Random random)
         {
             uint element = 0;
-            for (int i = 0; i < m; i++)
+            while (!(element*d%m==1))
             {
-                if ((e*d)%m==1)
-                {
-                    element = (uint)i;
-                    break;
-                }
+                element = (uint)random.Next((int)m - 1);
             }
+            //for (int i = 0; i < m; i++)
+            //{
+            //    if ((i*d)%m==1)
+            //    {
+            //        element = (uint)i;
+            //        break;
+            //    }
+            //}
             return element;
         }
-
+        public override string ToString()
+        {
+            return $"P={p} Q={Q} M={M} D={D} E={E} N={N}";
+        }
         public uint P { get => p; set => p = value; }
         public uint Q { get => q; set => q = value; }
         public uint M { get => m; set => m = value; }
         public uint D { get => d; set => d = value; }
-        public double E { get => e; set => e = value; }
+        public uint E { get => e; set => e = value; }
         public uint N { get => n; set => n = value; }
     }
 }
---
+++ RSACriptografic/ViewModels/ViewWorker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using RSACriptografic.Classes;
+
+namespace RSACriptografic.ViewModels
+{
+    class ViewWorker : INotifyPropertyChanged
+    {
+        static Random random = new Random();
+        CriptoWorker first;
+        public ViewWorker()
+        {
+            EncriptMessage = "";
+            first = new CriptoWorker(17, random);
+            StatusFirst = first.ToString().Replace(" ", Environment.NewLine);
+        }
+
+        public string StatusFirst
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string encriptMessage;
+        public string EncriptMessage
+        {
+            get { return encriptMessage; }
+            set
+            {
+                if (value == encriptMessage)
+                    return;
+                encriptMessage = value;
+                OnPropertyChanged("EncriptMessage");
+            }
+        }
+        private string unEncriptMessage;
+        public string UnEncriptMessage
+        {
+            get { return unEncriptMessage; }
+            set
+            {
+                if (value == unEncriptMessage)
+                    return;
+                unEncriptMessage = value;
+                OnPropertyChanged("UnEncriptMessage");
+            }
+        }
+        private string forEncript;
+        public string ForEncript
+        {
+            get { return forEncript; }
+            set { forEncript = value; }
+        }
+        public ICommand EncriptBtnClick
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    if (first!=null)
+                    {
+                        EncriptMessage = $@"{first.Encript(forEncript, first.E, first.N)}";
+                    }
+                });
+            }
+        }
+        public ICommand UnEncriptBtnClick
+        {
+            get
+            {
+                return new DelegateCommand((obj) =>
+                {
+                    if (first != null)
+                    {
+                        UnEncriptMessage = $@"{first.ToUnEncript(EncriptMessage)}";
+                    }
+                });
+            }
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
